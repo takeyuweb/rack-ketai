eb7f611f2f36bd984aa48318be6d62bbba2e16e4
Yuichi Takeuchi <info@takeyu-web.com>
2010-02-13 19:37:26 +0900

    Ruby 1.8.6以前への対応（IPAddr#to_range）

--- lib/rack/ketai/carrier/abstract.rb
+++ lib/rack/ketai/carrier/abstract.rb
@@ -6,6 +6,52 @@ require 'rack/request'
 require 'stringio'
 require 'ipaddr'
 
+unless IPAddr.instance_methods.include?("to_range")
+  class IPAddr
+
+    def coerce_other(other)
+      case other
+      when IPAddr
+        other
+      when String
+        self.class.new(other)
+      else
+        self.class.new(other, @family)
+      end
+    end
+
+    # Returns the successor to the ipaddr.
+    def succ
+      return self.clone.set(@addr + 1, @family)
+    end
+    
+    # Compares the ipaddr with another.
+    def <=>(other)
+      other = coerce_other(other)
+      
+      return nil if other.family != @family
+      
+      return @addr <=> other.to_i
+    end
+    include Comparable
+    
+    def to_range
+      begin_addr = (@addr & @mask_addr)
+      
+      case @family
+      when Socket::AF_INET
+        end_addr = (@addr | (IN4MASK ^ @mask_addr))
+      when Socket::AF_INET6
+        end_addr = (@addr | (IN6MASK ^ @mask_addr))
+      else
+        raise "unsupported address family"
+      end
+      return clone.set(begin_addr, @family)..clone.set(end_addr, @family)
+    end
+    
+  end
+end
+
 module Rack::Ketai::Carrier
   class Abstract
 
